024743cb126c31a61469d87064063700ef9369fb
cetecom <cetecom@LC08L1208>
2017-05-11 13:23:58 -0300

    Nuevo proyecto en dia lluvioso 3

--- modulos/prod_agregar.php
+++ modulos/prod_agregar.php
@@ -12,4 +12,14 @@ $ano=$_POST["ano"];
 $sql="INSERT INTO productos(nombre,totalusd,ano) values('$nom',$totusd, $ano)";
 
 echo $sql;
+    
+    if ($miconn->connect_errno) {
+             echo "Fallo al conectar a MySQL: (" . $miconn->connect_errno . ") " . $miconn->connect_error;
+             }
+            
+             /* Consultas de selección que devuelven un conjunto de resultados */
+             $resultado = $minconn->query($sql);
+                 
+                 $miconn->close();
 }
+          ?>
